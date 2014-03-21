92298027122fdf60fe6b83d8c2e39c15723926a7
Drew Volpe <accounts@dewdrops.net>
2014-03-21 13:10:57 -0400

    Made color selection not completely brain dead by sorting palette by average color value.

--- allcolors/color_replace.py
+++ allcolors/color_replace.py
@@ -1,12 +1,17 @@
 
 import argparse
 import math
+from operator import itemgetter
 from PIL import Image
 import random
 import sys
 import time
 
 MAX_COLOR = 2**8  # bit depth of the input image - TODO: compute automatically
+
+
+def avg(l):
+    return float(sum(l))/len(l) if len(l) > 0 else float('nan')
 
 
 def compute_delta(r1, g1, b1, r2, g2, b2):
@@ -23,22 +28,33 @@ def compute_delta(r1, g1, b1, r2, g2, b2):
 
 
 def find_closest_color(color, palette, max_tries=20000):
-    """ Assumes palette is sorted by sum of RBG, a crude measure of brightness
-        max_tries limits number of iterations to try.
+    """ max_tries limits number of iterations to try.
 
         TODO - This should use a sorted palette and do a binary search
     """
     if not palette:
         raise Exception('Palette empty - Ran out of colors; is input image too big ?')
-    best_color = None
+
+    color_avg = avg(color)
+    first_avg_match = 0
+    tup = 0
+    idx = 0
+    for idx, tup in enumerate(palette):  # todo - change to dict
+        if tup[3] >= color_avg:
+            first_avg_match = idx
+            break
+    idx = max(0, idx - (max_tries / 2))
+    sub_palette = palette[idx : idx + max_tries]
+
+    best_color = sub_palette[0]
     best_delta = sys.maxint
     tries = 0
-    for x, y, z in palette:
+    for x, y, z, a in sub_palette:
         if tries >= max_tries:
             return best_color
         cur_delta = compute_delta(color[0], color[1], color[2], x, y, z)
         if cur_delta < best_delta:
-            best_color = (x, y, z)
+            best_color = (x, y, z, a)
             best_delta = cur_delta
         tries += 1
     return best_color
@@ -52,8 +68,11 @@ def create_palette(color_depth=8, max_color_depth=8):
     for x in range(0, 2**color_depth):
         for y in range(0, 2**color_depth):
             for z in range(0, 2**color_depth):
-                palette.append( (x*scale, y*scale, z*scale) )
-    return palette
+                r = x*scale
+                g = y*scale
+                b = z*scale
+                palette.append( (r, g, b, int(avg([r,g,b]))) )
+    return sorted(palette, key=itemgetter(3))  # sort by avg
 
 
 def do_replace_random(im, palette):
@@ -67,7 +86,7 @@ def do_replace_random(im, palette):
     while (pixels_to_do):
         x, y = random.choice(pixels_to_do)
         new_color = find_closest_color(im.getpixel((x, y)), palette)
-        im.paste(new_color, box=((x, y, x+1, y+1)))
+        im.paste(new_color[0:3], box=((x, y, x+1, y+1)))
         palette.remove(new_color)
         pixels_to_do.remove((x, y))
 
@@ -97,7 +116,6 @@ if __name__ == "__main__":
     palette = create_palette(int(depth), MAX_COLOR)
     print 'Using palette with %s colors' % len(palette)
 
-    random.shuffle(palette)
     do_replace_random(im, palette)
 
     print 'Finished.  %s colors left in palette.' % len(palette)
