54f7f47ce07d941fc83f3b7d94b285764f9ca3fc
Drew Volpe <accounts@dewdrops.net>
2014-03-22 19:41:51 -0400

    Added max_tries parameter.

--- allcolors/color_replace.py
+++ allcolors/color_replace.py
@@ -7,7 +7,7 @@ import random
 import sys
 import time
 
-MAX_COLOR = 2**8  # bit depth of the input image - TODO: compute automatically
+MAX_COLOR_DEPTH = 2**8  # bit depth of the input image - TODO: compute automatically
 
 
 def avg(l):
@@ -27,9 +27,9 @@ def compute_delta(r1, g1, b1, r2, g2, b2):
         (sum1 - sum2)**2
 
 
-def find_closest_color(color, palette, max_tries=20000):
-    """ max_tries limits number of iterations to try.
-
+def find_closest_color(color, palette, max_tries):
+    """
+        max_tries limits number of iterations to try.
         TODO - This should use a sorted palette and do a binary search
     """
     if not palette:
@@ -39,10 +39,12 @@ def find_closest_color(color, palette, max_tries=20000):
     first_avg_match = 0
     tup = 0
     idx = 0
-    for idx, tup in enumerate(palette):  # todo - change to dict
+    for idx, tup in enumerate(palette):  # TODO optimize
         if tup[3] >= color_avg:
             first_avg_match = idx
             break
+
+    # look at max_tries colors closest to avg
     idx = max(0, idx - (max_tries / 2))
     sub_palette = palette[idx : idx + max_tries]
 
@@ -60,10 +62,10 @@ def find_closest_color(color, palette, max_tries=20000):
     return best_color
 
 
-def create_palette(color_depth=8, max_color_depth=8):
+def create_palette(color_depth=8):
     """ Create palette of all colors for color_depth bit rate.  """
     palette = []
-    scale = (max_color_depth / 2**color_depth)
+    scale = (MAX_COLOR_DEPTH / 2**color_depth)
 
     for x in range(0, 2**color_depth):
         for y in range(0, 2**color_depth):
@@ -75,9 +77,10 @@ def create_palette(color_depth=8, max_color_depth=8):
     return sorted(palette, key=itemgetter(3))  # sort by avg
 
 
-def do_replace_random(im, palette):
-    # Randomly select next pixel to replace
+def do_replace_random(im, palette, max_tries):
     start_time = time.time()
+
+    # Randomly select next pixel to replace
     pixels_to_do = []
     for x in range(0, im.size[0]):
         for y in range(0, im.size[1]):
@@ -85,7 +88,7 @@ def do_replace_random(im, palette):
     
     while (pixels_to_do):
         x, y = random.choice(pixels_to_do)
-        new_color = find_closest_color(im.getpixel((x, y)), palette)
+        new_color = find_closest_color(im.getpixel((x, y)), palette, max_tries)
         im.paste(new_color[0:3], box=((x, y, x+1, y+1)))
         palette.remove(new_color)
         pixels_to_do.remove((x, y))
@@ -97,13 +100,15 @@ def do_replace_random(im, palette):
 
 if __name__ == "__main__":
 
-
     parser = argparse.ArgumentParser(description='Create an AllRGB version of a given image')
     parser.add_argument('input_image', type=str, nargs=1, help='Input image')
+    parser.add_argument('--max_tries', dest='max_tries', default=20000,
+                        help='Maximum number of colors to look at per pixel.')
     args = vars(parser.parse_args())
     in_filename = args['input_image'][0]
 
     print 'Starting...'
+    print 'max_tries: %s' % args['max_tries']
     im = Image.open(in_filename)
     print 'Read file %s' % in_filename
 
@@ -113,10 +118,11 @@ if __name__ == "__main__":
     if depth != round(depth, 0) or depth < 3 or depth > 32:
         raise Exception('Unsupported image size: %s x %x\n' % (im.size[0], im.size[1]) +\
                         'Number of pixels must be a power of 2 between 3-32.')
-    palette = create_palette(int(depth), MAX_COLOR)
+    palette = create_palette(int(depth))
+
     print 'Using palette with %s colors' % len(palette)
 
-    do_replace_random(im, palette)
+    do_replace_random(im, palette, max_tries=args['max_tries'])
 
     print 'Finished.  %s colors left in palette.' % len(palette)
     out_filename = in_filename[:-4] + '_out.png'
